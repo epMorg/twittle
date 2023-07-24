1d66352c73b481577f40f1e976e5eb079320920a
epMorg <eoinmorg@gmail.com>
2023-07-24 23:33:32 +0100

    post font sizig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ const PostView = (props: PostWithUser) => {
       <div className="flex text-slate-400 gap-3">
         <span>{`@${author.username}`}</span><span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>      
       </div>
-      <span>{post.content}</span>
+      <span className="text-2xl">{post.content}</span>
     </div>   
   </div>
   );       
